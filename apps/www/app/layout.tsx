977fbe1d2340c3bcb280c1822e534b235aed273b
ruru <ruru.dev07@gmail.com>
2025-02-22 17:09:51 +0530

    feat(turbo): setup project with turbo to quick start

---
+++ apps/www/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+
+import "@stage/ui/globals.css";
+
+export const metadata: Metadata = {
+  title: "stage",
+  description: "let him cook!",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
+      <body className={GeistSans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ apps/www/tailwind.config.ts
@@ -0,0 +1,1 @@
+export * from "@stage/ui/tailwind.config";
